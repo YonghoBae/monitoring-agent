3c0c88618da92383324c7737f9d5a5194439d1c4
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-11-21 02:52:31 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/io/ohgnoy/monitoring_agent/MonitoringAgentApplication.java
@@ -0,0 +1,13 @@
+package io.ohgnoy.monitoring_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ohgnoy/monitoring_agent/MonitoringAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ohgnoy.monitoring_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
